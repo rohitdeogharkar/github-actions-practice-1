0b70892653b09a25160146179decd4162041c07f
rohit03deogharkar <rd5203327@gmail.com>
2026-02-14 23:38:00 +0530

    further more

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res)=>{
-    return res.json({msg : 'Hello from auto deploy serverrrrrrrrrrrrrrrrr v222222222222222222222222222'})
+    return res.json({msg : 'Hello from auto deploy serverrrrrrrrrrrrrrrrr its working -- CICD'})
 })
 
 app.listen(PORT, ()=>{
