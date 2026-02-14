c7f527f66c7c50478c2be6195e0734eee261e15f
rohit03deogharkar <rd5203327@gmail.com>
2026-02-14 23:02:07 +0530

    hello from serverrrrrrrrr

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res)=>{
-    return res.json({msg : 'Hello from local'})
+    return res.json({msg : 'Hello from serverrrrrrrrrr'})
 })
 
 app.listen(PORT, ()=>{
